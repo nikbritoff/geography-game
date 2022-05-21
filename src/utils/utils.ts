12ada6fd6c141e67a20372eff2108051d3cd6f0f
nikbritoff <britov.nd@gmail.com>
2022-05-21 21:59:11 +0300

    Исправление опечатки

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -9,7 +9,7 @@ const getRandomIntInclusive = (): number => {
   const MIN = -1;
   const MAX = 1;
 
-  return Math.floor(Math.random() * (MAX - MIN + 1) + MIN);
+  return Math.floor(Math.random() * (MAX - MIN + 1)) + MIN;
 }
 
 export const getRandomOrder = (data: Data): string[] => {
